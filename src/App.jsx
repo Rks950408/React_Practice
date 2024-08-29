8e8c21e67ea5636c7dd61e65c9cc23b1c8c8ea87
Rahul Kumar <rks950408@gmail.com>
2024-08-30 01:49:55 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import './App.css'
+import "./index.css";
 
 function App() {
-
   return (
     <>
-     
-      <h1 className='flex justify-center'>Vite + React</h1>
-     
+      <h1 className="flex  justify-end bg-indigo-700 text-white p-4">
+        Vite + React
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
